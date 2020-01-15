456bf1987c32fa61f92e0ae73dd9d6cc40f6bb93
Ben Godwin <ben.godwin1@gmail.com>
2020-01-15 13:59:36 -0500

    add ArrayChallenge

---
+++ section-8-arrays-lists-autoboxing-unboxing/ArrayChallenge/src/com/bengodwinweb/Main.java
@@ -0,0 +1,62 @@
+package com.bengodwinweb;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] integers = getIntegers();
+//	    int[] integers = {7, 11, 8, 2, 9};
+        System.out.println("integers: ");
+        printArray(integers);
+        int[] sortedInts = sortIntegers(integers);
+        System.out.println("sortedInts: ");
+        printArray(sortedInts);
+    }
+
+    public static int[] getIntegers() {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> ints = new ArrayList<Integer>();
+
+        System.out.println("Enter integers:");
+
+        while(true) {
+            boolean hasInt = scanner.hasNextInt();
+            if (!hasInt) break;
+            ints.add(scanner.nextInt());
+            scanner.nextLine();
+        }
+
+        System.out.println(ints);
+
+        scanner.close();
+        int[] integers = new int[ints.size()];
+        for (int i = 0; i < integers.length; i++) {
+            integers[i] = ints.get(i);
+        }
+        return integers;
+    }
+
+    public static int[] sortIntegers(int[] intArr) {
+        for (int i = 0; i < intArr.length - 1; i++) {
+            int a = intArr[i];
+            int b = intArr[i + 1];
+            if (a < b) {
+                intArr[i] = b;
+                intArr[i + 1] = a;
+                sortIntegers(intArr);
+            }
+        }
+        return intArr;
+    }
+
+    public static void printArray(int[] intArr) {
+        System.out.print("{");
+        for (int i : intArr) {
+            System.out.print(" " + i);
+        }
+        System.out.println(" }\n");
+    }
+}
