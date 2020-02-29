a817f5977119234031983ed0388bf558bc04e4ff
Ben Godwin <ben.godwin1@gmail.com>
2020-02-29 12:33:56 -0500

    add Controls

--- section-13-javafx/HelloWorldFX/src/module-info.java
+++ section-13-javafx/HelloWorldFX/src/module-info.java
@@ -1,6 +1,6 @@
 module HelloWorldFX {
-    requires javafx.fxml;
-    requires javafx.controls;
+        requires javafx.fxml;
+        requires javafx.controls;
 
-    opens sample;
-}
+        opens sample;
+        }
