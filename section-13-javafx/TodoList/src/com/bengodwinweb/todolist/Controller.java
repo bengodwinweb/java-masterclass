839136bb8e5fcf7986160b9dda470f44b6ea677c
Ben Godwin <ben.godwin1@gmail.com>
2020-02-29 15:48:26 -0500

    create TodoItem class and add intial todoItems to array

--- section-13-javafx/TodoList/src/com/bengodwinweb/todolist/Controller.java
+++ section-13-javafx/TodoList/src/com/bengodwinweb/todolist/Controller.java
@@ -1,4 +1,33 @@
 package com.bengodwinweb.todolist;
 
+import com.bengodwinweb.todolist.datamodel.TodoItem;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+
+    private List<TodoItem> todoItems;
+
+    public void initialize() {
+        TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 30th birthday card for John",
+                LocalDate.of(2020, Month.APRIL, 25));
+        TodoItem item2 = new TodoItem("Doctor's Appointment", "See Dr. Smith at 123 Main Street. Bring paperwork",
+                LocalDate.of(2020, Month.MAY, 10));
+        TodoItem item3 = new TodoItem("Finish design proposal for client", "I promised Mike I'd email website mockups by Friday March 6th",
+                LocalDate.of(2020, Month.MARCH, 6));
+        TodoItem item4 = new TodoItem("Pikcup Doug at the train station", "Doug's arriving on February 23 on the 5:00 train",
+                LocalDate.of(2020, Month.FEBRUARY, 23));
+        TodoItem item5 = new TodoItem("Pick up dry cleaning", "The clothes should be ready by Wednesday",
+                LocalDate.of(2020, Month.MARCH, 3));
+
+        todoItems = new ArrayList<TodoItem>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+        todoItems.add(item3);
+        todoItems.add(item4);
+        todoItems.add(item5);
+    }
 }
