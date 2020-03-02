2e84da9432e02bef703046d70f977c72d09bc0c4
Ben Godwin <ben.godwin1@gmail.com>
2020-03-01 19:53:35 -0500

    add ContextMenu for cells with Delete functionality

--- section-13-javafx/TodoList/src/com/bengodwinweb/todolist/Controller.java
+++ section-13-javafx/TodoList/src/com/bengodwinweb/todolist/Controller.java
@@ -4,6 +4,8 @@ import com.bengodwinweb.todolist.datamodel.TodoData;
 import com.bengodwinweb.todolist.datamodel.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
@@ -29,8 +31,23 @@ public class Controller {
     private Label deadlineLabel;
     @FXML
     private BorderPane mainBorderPane;
+    @FXML
+    private ContextMenu listContextMenu;
 
     public void initialize() {
+
+        listContextMenu = new ContextMenu();
+        MenuItem deleteMenuItem = new MenuItem("Delete");
+        deleteMenuItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+                deleteItem(item);
+            }
+        });
+
+        listContextMenu.getItems().addAll(deleteMenuItem);
+
         todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
             @Override
             public void changed(ObservableValue<? extends TodoItem> observableValue, TodoItem todoItem, TodoItem t1) {
@@ -70,6 +87,16 @@ public class Controller {
                     }
                 };
 
+                cell.emptyProperty().addListener(
+                        (obs, wasEmpty, isNowEmpty) -> {
+                            if (isNowEmpty) {
+                                cell.setContextMenu(null);
+                            } else {
+                                cell.setContextMenu(listContextMenu);
+                            }
+                        }
+                );
+
                 return cell;
             }
         });
@@ -102,6 +129,18 @@ public class Controller {
             System.out.println("OK PRESSED");
         } else {
             System.out.println("CANCEL PRESSED");
+        }
+    }
+
+    public void deleteItem(TodoItem item) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+
+        alert.setTitle("Delete Todo Item");
+        alert.setHeaderText("Delete Item: " + item.getShortDescription());
+        alert.setContentText("Are you sure? Press OK to confirm, or Cancel to back out");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            TodoData.getInstance().deleteTodoItem(item);
         }
     }
 }
