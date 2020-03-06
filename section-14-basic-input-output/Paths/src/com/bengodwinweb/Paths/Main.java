0313b365721311fa90a513efbeff02a18f6ce0d3
Ben Godwin <ben.godwin1@gmail.com>
2020-03-06 12:04:24 -0500

    read file attributes in Paths

--- section-14-basic-input-output/Paths/src/com/bengodwinweb/Paths/Main.java
+++ section-14-basic-input-output/Paths/src/com/bengodwinweb/Paths/Main.java
@@ -4,15 +4,39 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
-            Files.delete(fileToDelete);
+//            Path fileToCreate = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Files.createFile(fileToCreate);
+
+//            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir4");
+//            Files.createDirectory(dirToCreate);
+//
+//            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir2/Dir3/Dir4/Dir5/Dir6");
+//            Files.createDirectories(dirToCreate);
+
+            Path filePath = FileSystems.getDefault().getPath("Examples", "Dir1", "file1.txt");
+            System.out.println("Size = " + Files.size(filePath));
+            System.out.println("Last modified = " + formatFileTime(Files.getLastModifiedTime(filePath)));
+
+            BasicFileAttributes attrs = Files.readAttributes(filePath, BasicFileAttributes.class);
+            System.out.println("\nSize = " + attrs.size());
+            System.out.println("Last modified = " + formatFileTime(attrs.lastModifiedTime()));
+            System.out.println("Created = " + formatFileTime(attrs.creationTime()));
+            System.out.println("Is directory = " + attrs.isDirectory());
+            System.out.println("Is regular file = " + attrs.isRegularFile());
 
 
+//            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+//            Files.deleteIfExists(fileToDelete);
+//
 //            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1.txt");
 //            Path destination = FileSystems.getDefault().getPath("Examples", "file2.txt");
 //            Files.move(fileToMove, destination);
@@ -61,4 +85,9 @@ public class Main {
 //            System.out.println(e.getMessage());
 //        }
 //    }
+
+    public static String formatFileTime(FileTime time) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMM d, yyyy h:mm:ss a");
+        return time.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().format(dtf);
+    }
 }
