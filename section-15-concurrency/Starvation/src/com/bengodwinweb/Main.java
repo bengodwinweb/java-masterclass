b89bf44c09faf788ede198bb24255e3a5c602d3f
Ben Godwin <ben.godwin1@gmail.com>
2020-03-07 11:36:20 -0500

    add Starvation

---
+++ section-15-concurrency/Starvation/src/com/bengodwinweb/Main.java
@@ -0,0 +1,48 @@
+package com.bengodwinweb;
+
+
+import static com.bengodwinweb.ThreadColor.*;
+
+public class Main {
+    private final String RESET = ANSI_RESET;
+    private static Object lock = new Object();
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Worker(ANSI_RED), "Priority 10");
+        Thread t2 = new Thread(new Worker(ANSI_YELLOW), "Priority 8");
+        Thread t3 = new Thread(new Worker(ANSI_GREEN), "Priority 6");
+        Thread t4 = new Thread(new Worker(ANSI_CYAN), "Priority 4");
+        Thread t5 = new Thread(new Worker(ANSI_PURPLE), "Priority 2");
+
+        t1.setPriority(10);
+        t2.setPriority(8);
+        t3.setPriority(6);
+        t4.setPriority(4);
+        t5.setPriority(2);
+
+        t4.start();
+        t2.start();
+        t5.start();
+        t3.start();
+        t1.start();
+    }
+
+    private static class Worker implements Runnable {
+        private int runCount = 1;
+        private String threadColor;
+
+        public Worker(String threadColor) {
+            this.threadColor = threadColor;
+        }
+
+        @Override
+        public void run() {
+            for(int i = 0; i<100; i++) {
+                synchronized (lock) {
+                    System.out.format(threadColor + "%s: runCount = %d%n", Thread.currentThread().getName(), runCount++);
+                    // execute critical section of code
+                }
+            }
+        }
+    }
+}
