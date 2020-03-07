338f8334447f7d2adfea694949ad9e9fdb39ee3c
Ben Godwin <ben.godwin1@gmail.com>
2020-03-07 13:34:28 -0500

    uadd progressBar and progressLabel to TaskExample

--- section-15-concurrency/TaskExample/src/sample/Controller.java
+++ section-15-concurrency/TaskExample/src/sample/Controller.java
@@ -5,11 +5,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.BorderPane;
 
 public class Controller {
     @FXML
     private ListView listView;
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private Label progressLabel;
 
     private Task<ObservableList<String>> task;
 
@@ -17,26 +25,41 @@ public class Controller {
         task = new Task<ObservableList<String>>() {
             @Override
             protected ObservableList<String> call() throws Exception {
-                Thread.sleep(1000);
-
-                ObservableList<String> employees = FXCollections.observableArrayList(
+                String[] names = {
                         "Tim Buchalka",
                         "Bill Rogers",
                         "Jack Jill",
                         "Joan Andrews",
                         "Mary Johnson",
                         "Bob McDonald"
-                );
+                };
+
+                ObservableList<String> employees = FXCollections.observableArrayList();
+
+                int progress = 0;
+                int maxProgress = names.length;
+                for (String name : names) {
+                    employees.add(name);
+                    updateProgress(++progress, maxProgress);
+                    updateMessage(String.format("Added %s to the list", name));
+                    Thread.sleep(200);
+                }
+
+                progressBar.setVisible(false);
+                progressLabel.setVisible(false);
 
                 return employees;
             }
         };
 
+        progressBar.progressProperty().bind(task.progressProperty());
+        progressLabel.textProperty().bind(task.messageProperty());
         listView.itemsProperty().bind(task.valueProperty());
     }
 
     @FXML
     public void buttonPressed() {
+        progressBar.setVisible(true);
         new Thread(task).start();
     }
 }
