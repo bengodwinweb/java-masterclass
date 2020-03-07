038ed47cfd838ee72da9b55dcf027c875916db9f
Ben Godwin <ben.godwin1@gmail.com>
2020-03-07 12:07:27 -0500

    Add PoliteWorker

---
+++ section-15-concurrency/PoliteWorker/src/com/bengodwinweb/Main.java
@@ -0,0 +1,9 @@
+package com.bengodwinweb;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final Worker worker1 = new Worker("Worker1", true);
+        final Worker worker2 = new Worker("Worker2", true);
+    }
+}
