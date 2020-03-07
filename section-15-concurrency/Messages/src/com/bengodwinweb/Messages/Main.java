f2861c8aa5fc7172a3a6e261891cf15530f2fece
Ben Godwin <ben.godwin1@gmail.com>
2020-03-06 19:36:07 -0500

    finish Messages

--- section-15-concurrency/Messages/src/com/bengodwinweb/Messages/Main.java
+++ section-15-concurrency/Messages/src/com/bengodwinweb/Messages/Main.java
@@ -5,7 +5,9 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
     }
 }
 
@@ -15,18 +17,26 @@ class Message {
 
     public synchronized String read() {
         while(empty) {
-
+            try {
+                wait();
+            } catch(InterruptedException e) {
+            }
         }
         empty = true;
+        notifyAll();
         return message;
     }
 
     public synchronized void write(String message) {
         while(!empty) {
-
+            try {
+                wait();
+            } catch(InterruptedException e) {
+            }
         }
         empty = false;
         this.message = message;
+        notifyAll();
     }
 }
 
@@ -37,7 +47,6 @@ class Writer implements Runnable {
         this.message = message;
     }
 
-    @Override
     public void run() {
         String[] messages = {
                 "Humpty Dumpty sat on a wall",
@@ -53,10 +62,31 @@ class Writer implements Runnable {
             try {
                 Thread.sleep(random.nextInt(2000));
             }catch (InterruptedException e) {
-                System.out.println("Interrupted Exception: " + e.getMessage());
+                System.out.println("Writer Interrupted Exception: " + e.getMessage());
             }
-
-            message.write("Finished");
         }
+        message.write("Finished");
+    }
+}
+
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        Random random = new Random();
+        for(String latestMessage = message.read(); !latestMessage.equals("Finished");
+            latestMessage = message.read()) {
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch(InterruptedException e) {
+                System.out.println("Reader Interrupted Exception: " + e.getMessage());
+            }
+        }
+
     }
 }
