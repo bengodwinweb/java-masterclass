12a136086d1888dc96c83eaad35f4e4b753da86a
Ben Godwin <ben.godwin1@gmail.com>
2020-03-08 13:23:03 -0400

    add ReentrantLock to BankAccountChallenge

--- section-15-concurrency/BankAccountChallenge/src/com/bengodwinweb/BankAccount.java
+++ section-15-concurrency/BankAccountChallenge/src/com/bengodwinweb/BankAccount.java
@@ -1,30 +1,64 @@
 package com.bengodwinweb;
 
 import java.text.NumberFormat;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+import static com.bengodwinweb.Main.ANSI_RESET;
 import static com.bengodwinweb.Main.ANSI_YELLOW;
 
 public class BankAccount {
     private double balance;
     private String accountNumber;
     private NumberFormat nf = NumberFormat.getCurrencyInstance();
+    private Lock lock;
 
     public BankAccount(String accountNumber, double initialBalance) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
+        this.lock = new ReentrantLock();
     }
 
     public void deposit(double amount) {
-        synchronized (this) {
-            balance += amount;
-            System.out.println(ANSI_YELLOW + "balance after deposit on " + Thread.currentThread().getName() + " is " + nf.format(balance));
+        try {
+            if(lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance += amount;
+                    System.out.println(ANSI_YELLOW + "balance after deposit on " + Thread.currentThread().getName() + " is " + nf.format(balance));
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println(ANSI_YELLOW + Thread.currentThread().getName() + " could not get lock");
+            }
+        } catch(InterruptedException e) {
+            System.out.println(ANSI_RESET + "Interrupted exception: " + e.getMessage());
         }
     }
 
     public void withdraw(double amount) {
-        synchronized (this) {
-            balance -= amount;
-            System.out.println(ANSI_YELLOW + "balance after withdrawal on " + Thread.currentThread().getName() + " is " + nf.format(balance));
+        try {
+            if (lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance -= amount;
+                    System.out.println(ANSI_YELLOW + "balance after withdrawal on " + Thread.currentThread().getName() + " is " + nf.format(balance));
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println(ANSI_YELLOW + Thread.currentThread().getName() + " could not get lock");
+            }
+        } catch (InterruptedException e) {
+            System.out.println(ANSI_RESET + "Interrupted exception: " + e.getMessage());
         }
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void printAccountNumber() {
+        System.out.println("Account number: " + accountNumber);
     }
 }
