25fdc6444f7deb8c43d5fc93075632ee38621762
Ben Godwin <ben.godwin1@gmail.com>
2020-03-08 15:50:56 -0400

    add ButtonFX and lambda expression event handler

--- section-16-lambdas/ButtonFX/src/sample/Controller.java
+++ section-16-lambdas/ButtonFX/src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Button clickMeButton;
+
+    public void initialize() {
+        clickMeButton.setOnAction((e) -> System.out.println("Button Clicked"));
+    }
+
 }
