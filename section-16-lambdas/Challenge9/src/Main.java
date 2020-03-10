ab93f888de202519cc986434f4849ccc0d968b74
Ben Godwin <ben.godwin1@gmail.com>
2020-03-10 00:47:45 -0400

    add Challenge11 code to Challenge9 project

--- section-16-lambdas/Challenge9/src/Main.java
+++ section-16-lambdas/Challenge9/src/Main.java
@@ -18,10 +18,23 @@ public class Main {
                 "Jacob"
         );
 
+        // Print names in alphabetical order, with first letter capitalized
         topNames2015
                 .stream()
-                .sorted()
                 .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
+                .sorted()
                 .forEach(System.out::println);
+
+        System.out.println();
+
+        // Print out how many names begin with the letter 'A'
+        System.out.println("A names = " +
+                topNames2015
+                .stream()
+                .filter(s -> s.toUpperCase().startsWith("A"))
+                .count()
+        );
+
+
     }
 }
