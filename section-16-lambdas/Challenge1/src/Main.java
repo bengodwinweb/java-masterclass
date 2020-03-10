ab7909a1d8b9d44a54dd849b03df7b0b2f626e68
Ben Godwin <ben.godwin1@gmail.com>
2020-03-09 23:20:46 -0400

    finish Challenge1

---
+++ section-16-lambdas/Challenge1/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Runnable runnable = () -> {
+            String myString = "Let's split this up into an array";
+            String[] parts = myString.split(" ");
+            Arrays.stream(parts).forEach(System.out::println);
+        };
+
+        new Thread(runnable).start();
+    }
+}
