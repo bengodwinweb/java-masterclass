f33b32149b0037571c933e93e54d0d1343a4fb86
Ben Godwin <ben.godwin1@gmail.com>
2020-03-10 00:00:21 -0400

    complete Challenge2

--- section-16-lambdas/Challenge2/src/Main.java
+++ section-16-lambdas/Challenge2/src/Main.java
@@ -1,6 +1,34 @@
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Function<String, String> returnVal = s -> {
+            StringBuilder r = new StringBuilder();
+            for (int i=0; i<s.length(); i++) {
+                if (i % 2 == 0) {
+                    r.append(s.charAt(i));
+                }
+            }
+            return r.toString();
+        };
+        Function<String, String> spongebobCase = s -> {
+            StringBuilder r = new StringBuilder();
+            for (int i =0; i<s.length(); i++) {
+                if (i % 2 == 1) {
+                    s = s.toUpperCase();
+                } else {
+                    s = s.toLowerCase();
+                }
+                r.append(s.charAt(i));
+            }
+            return r.toString();
+        };
+
+
+        Stream.of("Ben Godwin", "Jake Kaplan").map(returnVal).forEach(System.out::println);
+        System.out.println();
+        Stream.of("Ben Godwin", "Jake Kaplan").map(spongebobCase).forEach(System.out::println);
     }
 }
