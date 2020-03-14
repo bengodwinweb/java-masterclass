6f4a8ee4bcce6cb64ad05dc1aeb1b2ec4b4be5ac
Ben Godwin <ben.godwin1@gmail.com>
2020-03-13 23:05:49 -0400

    add Challenge2

--- section-17-regex/Challenge/src/Main.java
+++ section-17-regex/Challenge/src/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         String challenge1 = "I want a bike.";
-        System.out.println(challenge1.matches("I want a bike\\."));
+        String challenge2 = "I want a ball.";
+
+        System.out.println("Challenge 1: " + challenge1.matches("I want a bike\\.") + "\n");
+        System.out.println("Challenge 2: " + (challenge1.matches("I want a b\\w{3}\\.") && challenge2.matches("I want a b\\w{3}\\.")));
+        System.out.println("Challenge 2: " + (challenge1.matches("I want a (bike|ball)\\.") && challenge2.matches("I want a (bike|ball)\\.")) + "\n");
     }
 }
